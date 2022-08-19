9099a08121398b6378053855f4be6fe954858f21
Bnepster <103311338+Bnepster@users.noreply.github.com>
2022-08-18 21:23:28 -0300

    atualizei

---
+++ exercicios/ex002.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um número '))
+n2 = int(input('Outro número '))
+s = n1 + n2
+d = n1 / n2
+m = n1 * n2
+di = n1 // n2
+e = n1 ** n2
+
+print(f'A soma é {s}, a multiplicação é {m}, a divisão inteira é {di}, a exponenciação {e} é divisão é {d}')
