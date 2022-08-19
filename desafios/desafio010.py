ec79d4f57fd591e223b65ca2735e44154fe8f4de
Bnepster <103311338+Bnepster@users.noreply.github.com>
2022-08-18 23:15:58 -0300

    atz

---
+++ desafios/desafio010.py
@@ -0,0 +1,5 @@
+p = float(input('preço do produto '))
+d = p / 20
+r = p - d
+
+print(f'O preço do produto com desconto é {r:.2f}')
---
+++ desafios/desafio011.py
@@ -0,0 +1,5 @@
+s = float(input('Qual seu salário '))
+p = s / 6.66
+ns = s + p
+
+print(f'Seu novo salário é de {ns:.2f}')
