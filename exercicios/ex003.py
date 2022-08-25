e0b0ce1753825e9d322886694cc46099f8f73f2a
Bnepster <103311338+Bnepster@users.noreply.github.com>
2022-08-25 12:15:45 -0300

    Atualização

---
+++ exercicios/ex003.py
@@ -0,0 +1,13 @@
+# Primeira maneira de importar bibliotecas
+
+# import math
+# num = int(input('Digite um número '))
+# raiz = math.sqrt(num)
+# print(f'A raiz quadrada de {num} é {raiz:.2f}')
+
+# Segunda maneira de importar bibliotecas
+
+from math import sqrt
+num = int(input('Digete um número '))
+raiz = sqrt(num)
+print(f'A raiz quadrada de {num} é {raiz}')
