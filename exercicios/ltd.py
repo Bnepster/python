d2e6f279c9478051d200b24561bc787cd589a93e
Bruno de Sousa <103311338+Bnepster@users.noreply.github.com>
2023-02-10 20:46:14 -0300

    Create ltd.py

---
+++ exercicios/ltd.py
@@ -0,0 +1,18 @@
+# Listas
+lista = [
+    'Bruno', 'Bianca', 'Brendon', 'Beatriz'
+]
+
+# Tuplas
+tupla = (
+    'Carro', 'Moto', 'Bicicletta'
+)
+
+# Dicionário
+dicionario = {
+    'coarro' : 'palio', 'moto' : 'fan' , 'bicicleta' : 'caloi'
+}
+
+print(dicionario)
+print(lista)
+print(tupla)
